e63586fcf4fdf980ab457104888cf56314bf8430
BuildTools <unconfigured@null.spigotmc.org>
2023-03-25 13:51:02 +0100

    Forgot a throws SQLException

--- src/main/java/it/tristana/commons/database/SqlConsumer.java
+++ src/main/java/it/tristana/commons/database/SqlConsumer.java
@@ -1,8 +1,9 @@
 package it.tristana.commons.database;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public interface SqlConsumer {
 
-	void accept(ResultSet resultSet);
+	void accept(ResultSet resultSet) throws SQLException;
 }
